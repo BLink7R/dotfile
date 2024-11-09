59d03309202a7659c14581bbe52f60d5d5b4867b
zuobulei <zuobulei@wps.cn>
2024-11-09 21:31:59 +0800

    edit for ink style

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     source (starship init fish --print-full-init | psub)
+    set Omen z131@192.168.0.101
+    set Kso Administrator@10.13.149.128
 end
