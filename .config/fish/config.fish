4500c5d424dee947669b4486f06a87bafccabb4b
zuobulei <zuobulei@wps.cn>
2024-11-11 10:19:25 +0800

    modify starship for dark and bright

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    source (/data/data/com.termux/files/usr/bin/starship init fish --print-full-init | psub)
+    source (starship init fish --print-full-init | psub)
     set Omen z131@192.168.0.101
     set Kso Administrator@10.13.149.128
 end
