38e0ef815e88eb6a8cbd2c7b61d752cb8c59a2b0
zuobulei <zuobulei@wps.cn>
2024-12-10 09:22:53 +0800

    change powershell color scheme

--- .config/ps1/Microsoft.PowerShell_profile.ps1
+++ .config/ps1/Microsoft.PowerShell_profile.ps1
@@ -8,4 +8,16 @@ function conda {
 
 & "$home\Documents\WindowsPowerShell\SamplePSReadLineProfile.ps1"
 
+Set-PSReadLineOption -Colors @{
+    Command          = "DarkBlue"
+    Comment          = "DarkGray"
+    Default          = "Gray"
+    Error            = "DarkRed"
+    InlinePrediction = "$([char]0x1b)[3;90m" # 斜体深灰
+    Keyword          = "DarkMagenta"
+    String           = "$([char]0x1b)[3;32m" # 斜体深绿
+    Variable         = "DarkCyan"
+    Number           = "Blue"
+}
+
 Invoke-Expression (&starship init powershell)
