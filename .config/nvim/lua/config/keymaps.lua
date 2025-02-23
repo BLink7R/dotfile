77dc1952958272884239fa13b8d33fce3ce3ca24
z131 <2437113492@qq.com>
2025-02-23 17:57:50 +0800

    add some key

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.api.nvim_set_keymap('n', 'gl', '$', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gs', '^', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'gl', '$', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'gs', '^', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', ')', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '(', ':bprevious<CR>', { noremap = true, silent = true })
--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -1,6 +1,17 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
 if true then return {
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      picker = {
+        sources = {
+          files = { hidden = true },
+        },
+      }
+    }
+  }
 } end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
