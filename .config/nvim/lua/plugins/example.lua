1e74b2f3d5228ae3d84f20f184821caadb1dcb6e
z131 <2437113492@qq.com>
2025-09-22 20:51:44 +0800

    modify lualine

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -37,47 +37,66 @@ return {
       { "<leader>wc", "<cmd>CopilotChatToggle<cr>", desc = "Copilot Chat Window", mode = { 'n', 'x' } },
     },
   },
-  -- {
-  --   "nvim-lualine/lualine.nvim",
-  --   event = "VeryLazy",
-  --   opts = function()
-  --     local opts = {
-  --       sections = {
-  --         lualine_a = { 'mode' },
-  --         lualine_b = { 'branch', 'diff' },
-  --         lualine_c = {
-  --           'filename',
-  --           {
-  --             'diagnostics',
-  --             symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
-  --             colored = false,
-  --             update_in_insert = false,
-  --             always_visible = false,
-  --           },
-  --           { LazyVim.lualine.pretty_path() },
-  --         },
-  --         lualine_x = {{
-  --             function() return require("noice").api.status.command.get() end,
-  --             cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-  --             color = function() return { fg = Snacks.util.color("Statement") } end,
-  --           },
-  --           { 'encoding', show_bomd = true, },
-  --           {
-  --             'fileformat',
-  --             symbols = { unix = '', dos = '', mac = '', }
-  --           },
-  --           'filetype',
-  --         },
-  --         lualine_y = {
-  --           { 'progress', separator = '', },
-  --           { 'location', separator = '', },
-  --         },
-  --         lualine_z = {
-  --           { 'datetime', icon = '', style = '%H:%M' },
-  --         },
-  --       },
-  --     }
-  --     return opts
-  --   end,
-  -- }
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function()
+      local opts = {
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff' },
+          lualine_c = {
+            'filename',
+            {
+              'diagnostics',
+              symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+              colored = false,
+              update_in_insert = false,
+              always_visible = false,
+            },
+          },
+          lualine_x = {
+            {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              separator = '',
+            },
+            { 'encoding', show_bomd = true, },
+            {
+              'fileformat',
+              symbols = { unix = '', dos = '', mac = '', }
+            },
+            'filetype',
+          },
+          lualine_y = {
+            { 'progress', separator = '', },
+            { 'location', separator = '', },
+          },
+          lualine_z = {
+            { 'datetime', icon = '', style = '%H:%M' },
+          },
+        },
+      }
+      
+      if vim.g.trouble_lualine and LazyVim.has("trouble.nvim") then
+        local trouble = require("trouble")
+        local symbols = trouble.statusline({
+          mode = "symbols",
+          groups = {},
+          title = false,
+          filter = { range = true },
+          format = "{kind_icon}{symbol.name:Normal}",
+          hl_group = "lualine_c_normal",
+        })
+        table.insert(opts.sections.lualine_c, {
+          symbols and symbols.get,
+          cond = function()
+            return vim.b.trouble_lualine ~= false and symbols.has()
+          end,
+        })
+      end
+
+      return opts
+    end,
+  }
 }
